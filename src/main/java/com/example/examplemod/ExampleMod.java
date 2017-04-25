fd8369974d2a2858242f72dca25aca5646ac4b19
actioninja <actioninja@gmail.com>
2017-04-24 21:19:56 -0700

    *DUBSTEP NOISES*

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class ExampleMod
 {
     public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "${version}";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
